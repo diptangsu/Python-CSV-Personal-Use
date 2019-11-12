afc4d5e006fd751cae5dbfe661463293342e5dfc
Diptangsu Goswami <diptangsu_goswami@persistent.co.in>
2019-11-12 12:09:16 +0530

    refactored code

--- split-csv-into-depts-and-years-pandas.py
+++ split-csv-into-depts-and-years-pandas.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 file = pd.read_csv('input/Python Skill Development Programme Registration FINAL.csv')
+number_of_students = open('csv-outputs/Number of Students.txt', 'w')
 
 depts = ['CSE', 'IT', 'ECE', 'EIE', 'EE', 'ME', 'MCA']
 years = 4
@@ -19,6 +20,17 @@ output_dfs = [
     for i in range(years)
 ]
 
+
+total_students = 0
 for row in output_dfs:
     for filename, df in row:
+        year = ' '.join(filename.split('-')[:2])
+        dept = filename.split("-")[2]
+        number_of_students.write(f'{year}: {dept}\t-> {len(df):3} students')
+        total_students += len(df)
+              
         df.to_csv(f'pandas-outputs\\{filename}.csv')
+
+    number_of_students.write('\n')
+
+number_of_students.write(f'Total Students: {total_students}')
